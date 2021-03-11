ae85e7b059180ee23acbd14d42fa50368bfd36bc
Kyuwoong Lee <kwooler@gmail.com>
2021-03-11 06:35:05 +0000

    Kyu

--- Assignment2-4.cpp
+++ Assignment2-4.cpp
@@ -3,23 +3,28 @@ using namespace std;
 
 int main()
 {
-  char c;
-  int i =65;
-    
-  while (i<91)
-  {
-      c=i;
+	char c;
+	//   int i =65;
+	c = 65;
+	while (c < 91)
+	{
+		cout << c << " ";
+		if (c % 5 == 4)
+			cout << " " << endl;
+		c++;
+	}
+//   while (c < 91)
+//   {
+//       if (c % 5 == 4)
+//       {
+//           cout << c << " " << endl;
+//       }
+//       else
+//       {
+//           cout << static_cast<char>(i)<< " ";
+//       }
         
-      if (c% 5==4)
-      {
-          cout << c << " " << endl;
-      }
-      else
-      {
-          cout << static_cast<char>(i)<< " ";
-      }
+//       i++;
         
-      i++;
-        
-  }
+//   }
 }
