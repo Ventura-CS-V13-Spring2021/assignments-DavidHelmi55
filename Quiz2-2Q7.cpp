0d4d687bd702cdea6a0e239c844afaab92add2fb
David Helmi <david_helmi1@my.vcccd.edu>
2021-03-22 18:25:46 +0000

    Check

---
+++ Quiz2-2Q7.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+    int EmployeeID1, EmployeeID2, EmployeeID3;
+    string EployeeName;
+    string Department;
+    double Salary;
+    
+    cout << "Enter all 3 eployyees ID's: ";
+    cin >> EmployeeID1 >> EmployeeID2 >> EmployeeID3;
+    
+}
