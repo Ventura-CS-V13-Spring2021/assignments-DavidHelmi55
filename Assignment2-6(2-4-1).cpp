8148d91e8b57c5a5ca7e101e24c93813e6db922c
David Helmi <david_helmi1@my.vcccd.edu>
2021-03-22 18:33:48 +0000

    Check

--- Assignment2-6(2-4-1).cpp
+++ Assignment2-6(2-4-1).cpp
@@ -1,23 +1,32 @@
 #include<iostream>
 #include<fstream>
-#include<cstdlib>
-#include<time.h>
 using namespace std;
 
 int main()
 {
-  srand(time(0));
-  int N;
+    
+  ofstream file("employee.txt");
 
-  ofstream file("data.txt");
+  int employees;
+  int EmployeeID1, EmployeeID2, EmployeeID3;
+  string EmployeeName1, EmployeeName2, EmployeeName3;
+  string Department1, Department2, Department3;
+  double Salary1, Salary2, Salary3;
+    
+  cout << "How many employees are there? ";
+  cin >> employees;
+    
+  cout << "Enter all 3 eployyees ID's: ";
+  cin >> EmployeeID1 >> EmployeeID2 >> EmployeeID3;
+    
+  cout << "Enter the employees names: ";
+  cin >> EmployeeName1 >> EmployeeName2 >> EmployeeName3;
+    
+  cout << "Enter the employees departments: ";
+  cin >> Department1 >> Department2 >> Department3;
   
-  cout<<"How many integers would you like?: ";
-  cin>>N;
-
-  int i;
-  for(i=0;i<N;i++)
-  {
-    file<<rand()%100<<endl;
-  }
+  cout << "Enter the employees salary: ";
+  cin >> Salary1 >> Salary2 >> Salary3;
+    
   file.close();
 }
