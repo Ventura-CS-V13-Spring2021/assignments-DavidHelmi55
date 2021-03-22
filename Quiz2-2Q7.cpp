df324859964c8f3535e48b2f6d0ded7b9cae5746
David Helmi <david_helmi1@my.vcccd.edu>
2021-03-22 18:36:53 +0000

    Check

--- Quiz2-2Q7.cpp
+++ Quiz2-2Q7.cpp
@@ -4,12 +4,29 @@ using namespace std;
 
 int main()
 {
-    int EmployeeID1, EmployeeID2, EmployeeID3;
-    string EployeeName;
-    string Department;
-    double Salary;
     
-    cout << "Enter all 3 eployyees ID's: ";
-    cin >> EmployeeID1 >> EmployeeID2 >> EmployeeID3;
+  ofstream file("employee.txt");
+
+  int employees;
+  int EmployeeID1, EmployeeID2, EmployeeID3;
+  string EmployeeName1, EmployeeName2, EmployeeName3;
+  string Department1, Department2, Department3;
+  double Salary1, Salary2, Salary3;
     
+  cout << "How many employees are there? ";
+  cin >> employees;
+    
+  cout << "Enter all 3 eployyees ID's: ";
+  cin >> EmployeeID1 >> EmployeeID2 >> EmployeeID3;
+    
+  cout << "Enter the employees names: ";
+  cin >> EmployeeName1 >> EmployeeName2 >> EmployeeName3;
+    
+  cout << "Enter the employees departments: ";
+  cin >> Department1 >> Department2 >> Department3;
+    
+  cout << "Enter the employees salary: ";
+  cin >> Salary1 >> Salary2 >> Salary3;
+    
+  file.close();
 }
