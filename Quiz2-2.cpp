342a8f0a3126314d07d0d77e932f15fca564e7bd
David Helmi <david_helmi1@my.vcccd.edu>
2021-03-22 17:56:18 +0000

    Check

--- Quiz2-2.cpp
+++ Quiz2-2.cpp
@@ -1,14 +1,23 @@
 #include<iostream>
-#include<cstdlib>
-#include<time.h>
 using namespace std;
 
 int main()
 {
   int num1;
+  int num2;
+  int num3;
+  int num4;
+  int num5;
     
-  num1 = rand()%10;
+  num1 = rand()%100;
+  num2 = rand()%100;
+  num3 = rand()%100;
+  num4 = rand()%100;
+  num5 = rand()%100;
     
   cout << num1;
+  cout << num2;
+  cout << num3;
+  cout << num4;
+  cout << num5;
 }
-
