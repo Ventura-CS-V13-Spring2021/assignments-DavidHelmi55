4ea9960e30be14d25b02ee2ba494991d0a632516
David Helmi <david_helmi1@my.vcccd.edu>
2021-03-22 17:31:31 +0000

    check

--- Quiz2-2.cpp
+++ Quiz2-2.cpp
@@ -1,13 +1,19 @@
 #include<iostream>
+#include<cstdlib>
+#include<time.h>
 using namespace std;
 
 int main()
 {
+  srand(time(0));
+  int N;
+  
+  cout<<"How many integers would you like?: ";
+  cin>>N;
 
-
-
-
-
-
-
+  int i;
+  for(i=0;i<N;i++)
+  {
+    cout<<rand()%99<<endl;
+  }
 }
