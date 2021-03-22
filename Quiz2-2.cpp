ca5289bd351e4446b7d9a8036cfaba5d7f1ef166
David Helmi <david_helmi1@my.vcccd.edu>
2021-03-22 17:51:23 +0000

    Check

--- Quiz2-2.cpp
+++ Quiz2-2.cpp
@@ -5,15 +5,10 @@ using namespace std;
 
 int main()
 {
-  srand(time(0));
-  int N;
-  
-  cout<<"How many integers would you like?: ";
-  cin>>N;
-
-  int i;
-  for(i=0;i<N;i++)
-  {
-    cout<<rand()%99<<endl;
-  }
+  int num1;
+    
+  num1 = rand()%10;
+    
+  cout << num1;
 }
+
