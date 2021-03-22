774ecd80aed10d68da738de90d4b2ffc0edf91e0
David Helmi <david_helmi1@my.vcccd.edu>
2021-03-22 18:39:58 +0000

    Check

---
+++ Quiz2-2Q7.2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int second()
+{
+    ofstream file(employee.txt);
+    
+    int totalsalary;
+    int avgsalary;
+    
+    totalsalary = Salary1 + Salary2 + Salary3
+    avgsalary = (Salary1 + Salary2 + Salary3) / 3
+}
