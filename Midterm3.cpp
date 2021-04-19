29072615b8c5f1d61112e2da3bfd9e0264444c14
David Helmi <>
2021-04-19 17:39:21 +0000

    .

--- Midterm3.cpp
+++ Midterm3.cpp
@@ -16,4 +16,31 @@ int main()
   num2 = (rand()%(1-9))+1;
   num3 = (rand()%(1-9))+1;
   
+  if (num1 > num2)
+  {
+    if (num1 > num3)
+      cout << "num1 is the largest, " << num1 << endl;
+    else
+      cout << "num2 is the largest, " << num2 << endl;
+  }
+    else{
+        if (num2 > num3)
+          cout << "num2 is the largest, " << num2 << endl;
+    else
+          cout << "num3 is the largest, " << num3 << endl;
+    }
+     
+  if (num1 < num2 && num1 < num3)
+  {
+    cout << "num1 is the smallest, " << num1 << endl;
+  }
+  else if (num2 < num1 && num2 < num3)
+  {
+    cout << "num2 is the smallest, " << num1 << endl;
+  }
+  else
+  {
+    cout << "num3 is the smallest, " << num1 << endl;
+  }
+  return 0;
 }
