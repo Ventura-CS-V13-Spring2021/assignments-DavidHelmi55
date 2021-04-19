2b7dd8c3faabf4b6600fbaba861def1da696c960
David Helmi <>
2021-04-19 16:57:38 +0000

    .

---
+++ Midterm2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main() 
+{
+  ofstream file("numbers.txt");
+    
+  srand((unsigned) time(0));
+  int randomNumber;
+  for (int index = 0; index < 10; index++) 
+  {
+    randomNumber = (rand() % 50) + 1;
+    file << randomNumber << endl;
+  }
+  
+}
