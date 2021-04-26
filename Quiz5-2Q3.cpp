08b499976484de3001a9c645eddb7f67c9fd3225
David Helmi <>
2021-04-26 22:34:31 +0000

    .

--- Quiz5-2Q3.cpp
+++ Quiz5-2Q3.cpp
@@ -5,26 +5,34 @@ void printbinary(int [], int);
 void count0cluster(int [], int);
 int main()
 {
-    const int SIZE = 20;
-    int     binary[SIZE];
+  const int SIZE = 20;
+  int     binary[SIZE];
 
-    srand(time(0));
-    makebinary(binary, SIZE);
-    printbinary(binary, SIZE);
-    count0cluster(binary, SIZE);
+  srand(time(0));
+  makebinary(binary, SIZE);
+  printbinary(binary, SIZE);
+  count0cluster(binary, SIZE);
 }
 void makebinary(int num[], int size)
 {
-    for(int i=0;i<size; i++)
-        num[i] = rand() % 2;
+  for(int i=0;i<size; i++)
+    num[i] = rand() % 2;
 }
 void printbinary(int num[], int size)
 {
-    for(int i=0;i<size; i++)
-        cout << num[i] << " ";
-    cout << endl;
+  for(int i=0;i<size; i++)
+    cout << num[i] << " ";
+  cout << endl;
 }
 void count0cluster(int bin[], int size)
 {
-  
+  int count = 0;
+  if (bin[0] == 0)
+        count++;
+
+  for (int i=1; i<20; ++i){
+    if (bin[i] == 0 and bin[i - 1] != 0)
+      count++;
+  }
+  cout << "Count of cluster: " << count << endl;
 }
