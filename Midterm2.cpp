01b7def11655b41f499945fcdbefb18deebfd867
David Helmi <>
2021-05-02 20:22:13 +0000

    Changes to Midterm2

--- Midterm2.cpp
+++ Midterm2.cpp
@@ -4,16 +4,42 @@
 #include<ctime>
 using namespace std;
 
-int main() 
+int prevNum = 51;
+
+int getRdnum()
 {
-  ofstream file("numbers.txt");
-    
   srand((unsigned) time(0));
   int getRdnum;
   for (int index = 0; index < 10; index++) 
   {
     getRdnum = (rand() % 50) + 1;
-    file << getRdnum << endl;
   }
-  
+}
+
+int isGreater(int n)
+{
+  if(n > prevNum)
+  return 1; 
+  else
+  return 0;
+}
+
+int main () 
+{
+  ofstream file("numbers.txt");
+  file.open ("numbers.txt");
+    
+  int n;
+  for(int i=0; i<n; i++)
+  {
+    int currentNum = getRdnum(); 
+    int check = isGreater(currentNum);
+    prevNum = currentNum; 
+    if (check == 1)
+      {
+        file.write(currentNum);
+      }
+  }
+  file.close(); 
+  return 0;
 }
