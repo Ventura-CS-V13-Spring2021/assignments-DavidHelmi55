db8335508559daed51682ac20ad1f09e23ad3db0
David Helmi <>
2021-05-10 23:39:36 +0000

    .

--- Quiz 7/rectangle.cpp
+++ Quiz 7/rectangle.cpp
@@ -17,5 +17,30 @@ Rectangle::Rectangle(Coordinate lbval, Coordinate rtval)
 
 Coordinate Rectangle::getLB() const
 {
-  
+  return lb;
+}
+
+Coordinate Rectangle::getRT() const
+{
+  return lb;
+}
+
+double Rectangle::getArea()
+{
+  return area;
+}
+
+Coordinate Rectnagle::getCenter()
+{
+  return center;
+}
+
+void Rectangle::setLBRT(Coordinate lbval, Coordinate rtval)
+{
+  lb = lbval;
+  rt = rtval;
+  area = (rt.getX() - lbgetX()) * (rt.getY() - lb.getY());
+  double c_x = (rt.getX() + lb.getX())/2;
+  double c_y = (rt.getY() + lb.getY())/2;
+  center.setXY(c_x,c_y);
 }
