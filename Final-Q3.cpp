aee3e2e11efb391119f910a5c5fa5dcff6c2a7b2
David Helmi <>
2021-05-18 17:12:00 +0000

    .

--- Final-Q3.cpp
+++ Final-Q3.cpp
@@ -11,7 +11,55 @@ class Student
   Student()
   {
     cout<<"Default constructor called\n";
-    name=" ";
+    name="";
     num_classes=0;
     class_List = NULL;
+  }
+
+  Student(const Student &st1)
+  {
+    cout<<"Copy constructor called\n";
+    name = st1.name;
+    num_classes = st1.num_classes;
+    class_List = st1.class_List;
+  }
+
+  Student(string n, int num)
+  {
+    cout<<"Parameterized constructor called\n";
+    name=n;
+    num_classes=num;
+    class_List = new string[num_classes];
+        
+    for(int i=0;i<num_classes;++i)
+    {
+      cout<<"Enter the course name: ";
+      cin>>class_List[i];
+    }
+  }
+
+  void get_input()
+  {
+    cout<<"Enter Name: ";
+    cin>>name;
+    cout<<"Enter number of enrolled courses: ";
+    cin>>num_classes;
+    class_List = new string[num_classes];
+        
+    for(int i=0;i<num_classes;++i)
+    {
+      cout<<"Enter the course name: ";
+      cin>>class_List[i];
+    }
+  }
+
+  void Print()
+  {
+    cout<<"Name: "<<name<<"\n";
+    cout<<"Enrolled Courses: \n";
+        
+    for(int i=0;i<num_classes;++i)
+    {
+      cout<<class_List[i]<<"\n";
+    }
   }
