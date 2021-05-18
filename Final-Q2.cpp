d8b0e4f24e7fd0f6c4c8bd8805b647e381e4395e
David Helmi <>
2021-05-18 16:48:55 +0000

    .

--- Final-Q2.cpp
+++ Final-Q2.cpp
@@ -22,3 +22,21 @@ int main()
       }
     }
   }
+
+  int count=1;
+  cout<<"N Count"<<endl;
+  for(int i=0; i<n-1; i++)
+  {
+    if(arr[i]==arr[i+1])
+    {
+      count++;
+    }
+    else
+    {
+      cout<<arr[i]<<" "<<count<<endl;
+      count=1;
+    }
+  }
+  cout<<arr[n-1]<<" "<<count<<endl;
+  return 0;
+}
