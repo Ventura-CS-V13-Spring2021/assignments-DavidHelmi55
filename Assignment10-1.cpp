d2a3bc7ee2d3d719ed01a272c7b025d8fb3d1889
David Helmi <>
2021-05-18 15:59:20 +0000

    .

--- Assignment10-1.cpp
+++ Assignment10-1.cpp
@@ -1,88 +1,114 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
 using namespace std;
 
-class student
+class Student
 {
-  public:
+    public:
+    Student();
+    ~Student();
+    void InputValue(); 
+    void OutputValue(); 
+    void ResetClasses(); 
+    Student& operator =(const Student& rightSide);
+
+    private:
     string name;
     int numClasses;
-    vector<string>classList;
-    student()
-    {
-      name="",
-      numClasses=0;
-    }
-    ~Student()
-    {
-      name;
-      numClasses;
-      classList;
-    }
-    void setName(string n)
-    {
-      name = n;
-    }
-    void setNumClasses(int sum)
-    {
-      numClasses = num;
-    }
-    string getName()
-    {
-      return name;
-    }
-    int getNumClasses()
-    {
-      return numClasses;
-    }
-    void input()
-    {
-      string name;
-      int num;
-      cout << "Enter student name: ";
-      cin >> name;
-      setName(name);
-      cout << "Enter number of classes: "
-      cin >> num;
-      cout << "Enter class names (Type end to stop): ";
-      string className;
-      while(true)
-      {
-        cin>>className;
-        if(className == "end")
-          break;
-        classList.push_back(className);
-      }
-
-    }
-    void show_info()
-    {
-      cout<<"Name of student: " << getName() << "\n";
-      cout<<"List of classes: \n";
-      for(int i=0;i<classList.size();i++)
-        cout << classList[i] << "\n";
-    }
-    void operator = (const Student &s)
-    {
-      name = s.name;
-      numClasses = s.numClasses;
-      classList = s.classList;
-    }
-    void reset()
-    {
-      setNumClasses(0);
-      classList.empty();
-    }
+    string *classList;
 };
+
+Student::Student()
+{
+    numClasses = 0;
+    classList = NULL;
+    name = "";
+}
+
+Student::~Student()
+{
+    if (classList != NULL)
+    {
+        delete [] classList;
+    }
+}
+
+void Student::ResetClasses()
+{
+    if (classList != NULL)
+    {
+        delete [] classList;
+        classList = NULL;
+    }
+    numClasses = 0;
+}
+
+void Student::InputValue()
+{
+    int i;
+    ResetClasses();
+    cout << "Please Enter student name." << endl;
+    getline(cin, name);
+    cout << "Please Enter number of classes." << endl;
+    cin >> numClasses;
+    cin.ignore(2,'\n'); 
+    if (numClasses > 0)
+    {
+        classList = new string[numClasses];
+        for (i=0; i<numClasses; i++)
+        {
+            cout << "Please Enter name of class " << (i+1) << endl;
+            getline(cin, classList[i]);
+        }
+    }
+    cout<<endl;
+}
+
+void Student::OutputValue()
+{
+    int i;
+    cout << "Name: " << name << endl;
+    cout << "Number of classes: " << numClasses << endl;
+    for (i=0; i<numClasses; i++)
+    {
+        cout << " Class " << (i+1) << ":" << classList[i] << endl;
+    }
+    cout << endl;
+}
+
+Student& Student::operator =(const Student& rightSide)
+{
+    int i;
+    ResetClasses();
+    name = rightSide.name;
+    numClasses = rightSide.numClasses;
+    if (numClasses > 0)
+    {
+        classList = new string[numClasses];
+        for (i=0; i<numClasses; i++)
+        {
+            classList[i] = rightSide.classList[i];
+        }
+    } 
+    return *this;
+}
 
 int main()
 {
-  Student s1;
-  s1.input();
-  s1.show_info();
-
-  Student s2;
-  s2.input();
-  s2.show_info();
-  s1 = s2
-  s1.show_info();
+    Student s1, s2;
+    s1.InputValue(); 
+    cout << "Student 1's data:" << endl;
+    s1.OutputValue(); 
+    cout << endl;
+    s2 = s1;
+    cout << "Student 2's data after assignment from student 1:" << endl;
+    s2.OutputValue();
+    s1.ResetClasses();
+    cout << "Student 1's data after reset:" << endl;
+    s1.OutputValue();
+    cout << "Student 2's data, should still have original classes:" << endl;
+    s2.OutputValue();
+    
+    return 0;
 }
